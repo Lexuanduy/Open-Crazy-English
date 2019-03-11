b61ccafe86309804a7af81e0ed25bd42e0ce0633
lexuanduy <lexuanduy07@gmail.com>
2019-03-11 18:51:03 +0700

    login with facebook

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { auth } from 'firebase/app';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +11,13 @@ import { Observable } from 'rxjs';
 })
 export class AppComponent {
   users: Observable<any[]>;
-  constructor(db: AngularFirestore) {
+  constructor(db: AngularFirestore, public afAuth: AngularFireAuth) {
     this.users = db.collection('users').valueChanges();
+  }
+  login() {
+    this.afAuth.auth.signInWithPopup(new auth.GoogleAuthProvider());
+  }
+  logout() {
+    this.afAuth.auth.signOut();
   }
 }
